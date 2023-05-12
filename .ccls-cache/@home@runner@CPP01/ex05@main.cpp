6304eabfb4c0c6e80f501396c04d0e9e83571928
pawaka2020 <40174427+pawaka2020@users.noreply.github.com>
2023-05-12 05:41:43 +0000

    fix

---
+++ .ccls-cache/@home@runner@CPP01/ex05@main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riramli <riramli@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 00:00:00 by riramli           #+#    #+#             */
+/*   Updated: 2023/03/03 00:00:00 by riramli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Harl.hpp"
+#include <string>
+#include <iostream>
+
+int main()
+{
+	Harl harl;
+	std::string input;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("DANCE");
+}
